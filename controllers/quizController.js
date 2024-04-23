2618b79d705a156d5d5996009e2a0f76d24f391f
yev <yevhen.domin.uk@gmail.com>
2024-04-23 15:43:38 +0100

    prettier

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -1,24 +1,34 @@
-const { getRandomString } = require('../helpers/index');
-const { createNewQuiz, createNewQuestion, createNewOption } = require('../db/queries');
+const { getRandomString } = require("../helpers/index");
+const {
+  createNewQuiz,
+  createNewQuestion,
+  createNewOption,
+} = require("../db/queries");
 
 const createQuiz = async (req, res) => {
-	const { title, questionText, options } = req.body;
-	console.log(options);
+  const { title, questionText, options } = req.body;
+  console.log(options);
 
-	const accessCode = await getRandomString();
-	const quizId = await createNewQuiz(title, accessCode);
-	const questionId = await createNewQuestion(title, questionText, quizId);
+  const accessCode = await getRandomString();
+  const quizId = await createNewQuiz(title, accessCode);
+  const questionId = await createNewQuestion(title, questionText, quizId);
 
-	const optionPromises = options.map(async (option) => {
-		const { min, max, answer } = option;
-		return createNewOption(min, max, answer, questionId, option.correct === 'on');
-	});
+  const optionPromises = options.map(async (option) => {
+    const { min, max, answer } = option;
+    return createNewOption(
+      min,
+      max,
+      answer,
+      questionId,
+      option.correct === "on",
+    );
+  });
 
-	await Promise.all(optionPromises);
+  await Promise.all(optionPromises);
 
-	return res.send(`Question created successfully! Access code: ${accessCode}`);
+  return res.send(`Question created successfully! Access code: ${accessCode}`);
 };
 
 module.exports = {
-	createQuiz,
+  createQuiz,
 };
--- helpers/index.js
+++ helpers/index.js
@@ -1,13 +1,14 @@
 const getRandomString = function () {
-	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	const codeLength = 6;
-	let str = '';
-	for (let i = 0; i < codeLength; i++) {
-		str += characters.charAt(Math.floor(Math.random() * characters.length));
-	}
-	return str;
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const codeLength = 6;
+  let str = "";
+  for (let i = 0; i < codeLength; i++) {
+    str += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return str;
 };
 
 module.exports = {
-	getRandomString,
+  getRandomString,
 };
