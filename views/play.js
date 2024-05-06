f03e1da00864b80c0fe3a77de44410be75c2c80a
yev <yevhen.domin.uk@gmail.com>
2024-05-06 11:30:53 +0100

    Only one radio button can be chosen

--- views/play.js
+++ views/play.js
@@ -21,13 +21,13 @@ const getPlayHTML = (questions) => {
         <div class="smallBox">
             <h1>${question.title}</h1>
             <p>${question.questionText}</p>
-            <input type="radio" value="${question.options[0].min} - ${question.options[0].max}" id="option1" />
+            <input type="radio" name="${question.id}" value="${question.options[0].min} - ${question.options[0].max}" id="option1" />
             <label for="option1">${question.options[0].min} - ${question.options[0].max}</label><br /><br />
-            <input type="radio" value="${question.options[1].min} - ${question.options[1].max}" id="option2" />
+            <input type="radio" name="${question.id}" value="${question.options[1].min} - ${question.options[1].max}" id="option2" />
             <label for="option2">${question.options[1].min} - ${question.options[1].max}</label><br /><br />
-            <input type="radio" value="${question.options[2].min} - ${question.options[2].max}" id="option3" />
+            <input type="radio" name="${question.id}" value="${question.options[2].min} - ${question.options[2].max}" id="option3" />
             <label for="option3">${question.options[2].min} - ${question.options[2].max}</label><br /><br />
-            <input type="radio" value="${question.options[3].min} - ${question.options[3].max}" id="option4" />
+            <input type="radio" name="${question.id}" value="${question.options[3].min} - ${question.options[3].max}" id="option4" />
             <label for="option4">${question.options[3].min} - ${question.options[3].max}</label><br/><br/>
         </div>`;
   });
