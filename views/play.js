8c8c9454356788eb3223e9f1ad58f0ff378c632b
yev <yevhen.domin.uk@gmail.com>
2024-05-09 17:17:17 +0100

    options position adjusted

--- views/play.js
+++ views/play.js
@@ -1,5 +1,5 @@
 const getPlayHTML = (questions, base64ImgMap, accessCode, user) => {
-  const headHTML = `<!doctype html>
+	const headHTML = `<!doctype html>
 <html>
   <head>
     <meta charset="UTF-8" />
@@ -10,48 +10,43 @@ const getPlayHTML = (questions, base64ImgMap, accessCode, user) => {
   <body>
     <form id="questions">`;
 
-  const bottomHTML = `
+	const bottomHTML = `
             <button type="submit" class="smallButton2">Submit</button>
-        </form>
-        <script src="submitQuiz.js"></script>
-    </body>
+    </form>
+      <script src="submitQuiz.js"></script>
+  </body>
 </html>;`;
 
-  //console.log(questions);
-  let questionsComponents = questions.map((question) => {
-    //console.log('QUESTION: \n', question);
-    // console.log(base64ImgMap);
-    //console.log('OPTIONS :', question.options);
-    let img = "";
-    if (base64ImgMap[question.id]) {
-      const { mimeType, imgBase64 } = base64ImgMap[question.id];
-      img = `<div id="img"><img src="data:${mimeType};base64,${imgBase64}" alt="Card Image${question.id}" style="width:500px;"></div>`;
-    }
+	let questionsComponents = questions.map((question) => {
+		let img = '';
+		if (base64ImgMap[question.id]) {
+			const { mimeType, imgBase64 } = base64ImgMap[question.id];
+			img = `<div id="img"><img src="data:${mimeType};base64,${imgBase64}" alt="Card Image${question.id}" style="width:450px;"></div>`;
+		}
 
-    //console.log(JSON.stringify(question));
-    return `
+		return `
         <div class="smallBox" id="${question.id}" quiz="${accessCode}" userName="${user}">
             <h1>${question.title}</h1>
             ${img}
             <p>${question.questionText}</p>
-            <div> 
+            <div class="option-column">
               <input type="radio" name="${question.id}" value="${question.options[0].id}" id="option1" />
-              <label for="option1">${question.options[0].min} - ${question.options[0].max}</label><br /><br/>
+              <label for="option1">${question.options[0].min};  ${question.options[0].max};</label><br /><br/>
               <input type="radio" name="${question.id}" value="${question.options[1].id}" id="option2" />
-              <label for="option2">${question.options[1].min} - ${question.options[1].max}</label><br /><br/>
+              <label for="option2">${question.options[1].min};  ${question.options[1].max};</label><br /><br/>
             </div>
-            <div>
+            <div class="option-column">
               <input type="radio" name="${question.id}" value="${question.options[2].id}" id="option3" />
-              <label for="option3">${question.options[2].min} - ${question.options[2].max}</label><br /><br/>
+              <label for="option3">${question.options[2].min};  ${question.options[2].max};</label><br /><br/>
               <input type="radio" name="${question.id}" value="${question.options[3].id}" id="option4" />
-              <label for="option4">${question.options[3].min} - ${question.options[3].max}</label><br/><br/>
+              <label for="option4">${question.options[3].min};  ${question.options[3].max};</label><br/><br/>
             </div>
         </div>`;
-  });
+	});
 
-  return headHTML.concat(questionsComponents.join(""), bottomHTML);
+	return headHTML.concat(questionsComponents.join(''), bottomHTML);
 };
 
 module.exports = {
-  getPlayHTML,
+	getPlayHTML,
 };
